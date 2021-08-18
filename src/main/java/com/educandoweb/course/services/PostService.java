3bdd3b669ad2d62ff04f98238c874c243097c6f0
Caio Cesar <caiocesar9920@gmail.com>
2021-08-18 17:03:17 -0300

    Simple query with 'query methods'

--- src/main/java/com/educandoweb/course/services/PostService.java
+++ src/main/java/com/educandoweb/course/services/PostService.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +16,13 @@ public class PostService {
 	@Autowired
 	private PostRepository postRepository;
 	
-
 	public Post findById(String id) {
 		Optional<Post> obj = postRepository.findById(id);
 		return obj.orElseThrow(()-> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public List<Post> findByTitle(String text){
+		return postRepository.findByTitleContainingIgnoreCase(text);
 	}
 }
 	
